2dc70d90cec4b2d929d0ccac868b9cddb9ac4a47
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-24 06:45:57 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
-def "display_rainbow" do
- puts colors=["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+display_rainbow= "colors"
+
+def display_rainbow
+  puts colors=["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 end
-colors
